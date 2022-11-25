c1fe9deeb1d95f48b6b81237ef3ff027c53286dd
Jayyue <jayyu1w@gmail.com>
2022-11-26 02:05:40 +0800

    fix banner height

--- front-Webpage/course_info.js
+++ front-Webpage/course_info.js
@@ -1,5 +1,5 @@
 $(window).scroll(function () {
-    if ($(window).scrollTop() <= 200) {
+    if ($(window).scrollTop() <= 250) {
         $(".navbar").removeClass("fixed-top");
     } else {
         $(".navbar").addClass("fixed-top");
