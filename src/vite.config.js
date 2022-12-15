ec2ae67ba0e8034c58f8b1cf485ae67c7ababd1c
Jayyue <jayyu1w@gmail.com>
2022-12-16 03:58:13 +0800

    reorganize files and trying to set github page

--- src/vite.config.js
+++ src/vite.config.js
@@ -15,11 +15,9 @@ export default defineConfig({
       input: {
         main: resolve(root, 'index.html'),
         all_course: resolve(root, 'all_course', 'index.html'),
-        bulletin: resolve(root, 'bulletin', 'index.html'),
-        course_edit: resolve(root, 'course_edit', 'index.html'),
-        course_info: resolve(root, 'course_info', 'index.html'),
         login: resolve(root, 'login', 'index.html'),
-      }
+        bulletin: resolve(root, 'bulletin', 'bulletin.html'),
+      },
     }
   }
 })
